340e3dab839c4c3fbb2a5eb291e240368528b331
jung <jungno@gmail.com>
2025-07-28 00:26:06 -0600

    Set Up the Mastra Instance and Vector Store

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -5,9 +5,17 @@ import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
 
+import { PgVector } from '@mastra/pg';
+import { researchAgent } from './agents/researchAgent';
+
+const pgVectorStore = new PgVector({
+  connectionString: process.env.POSTGRES_CONNECTION_STRING!, //! for trustme
+});
+
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
+  vectors: { pgVectorStore }, //vectors
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
